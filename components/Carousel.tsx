bbca9d30630ebb258fa130d7c5d53df401131a34
karisilva <128189992+karisilva@users.noreply.github.com>
2025-08-14 16:31:21 +0000

    adicionar carrosel

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -32,8 +32,8 @@ const Carousel = () => {
         <section className="section" style={{ minHeight: '80vh' }}>
           <div className="bg-gray container section shadow rounded-lg px-4">
             <div className="row align-items-center justify-content-center text-center text-md-left">
-              
-              <div className="col-lg-5 col-md-6 d-flex flex-column justify-content-center">
+
+              <div className="col-lg-6 col-md-6 d-flex flex-column justify-content-center">
                 <h2 className="section-title">PET Elétrica UFBA</h2>
                 <p className="mb-4 text-justify">
                   PET é a sigla para Programa de Educação Tutorial, um programa ligado
@@ -43,26 +43,32 @@ const Carousel = () => {
                   realizada por eles são orientadas por três pilares:{" "}
                   <b>Ensino, Extensão e Pesquisa</b>.
                 </p>
+
                 <div className="mt-auto">
                   <Link href="/team" passHref>
-                    <a title="Conheça nosso time" className="btn btn-primary">
+                    <a
+                      title="Conheça nosso Time"
+                      className="btn btn-primary">
                       Conheça nosso Time
                     </a>
                   </Link>
                 </div>
               </div>
 
-              <div className="col-lg-6 col-md-6 mb-4 mb-md-0">
+
+              <div className="col-lg-4 col-md-5 mb-4 mb-md-0">
                 <div className="img-fluid">
                   <Image 
-                    src="/images/banner-art.svg" 
-                    alt="Arte do banner PET Elétrica"
+                    src="/images/cta.svg" 
+                    alt="Ilustração do Processo Seletivo" 
                     layout='responsive' 
                     width="100%" 
-                    height="100%"
+                    height="100%" 
                   />
                 </div>
               </div>
+
+
             </div>
           </div>
         </section>
@@ -72,7 +78,95 @@ const Carousel = () => {
 
 
       {/* ===== SLIDE 2: PROCESSO SELETIVO ===== */}
-      <SwiperSlide">
+      <SwiperSlide>
+        <section className="section" style={{ minHeight: '80vh' }}>
+          <div className="bg-gray container section shadow rounded-lg px-4">
+            <div className="row align-items-center justify-content-center text-center text-md-left">
+
+              <div className="col-lg-4 col-md-5 mb-4 mb-md-0">
+                <div className="img-fluid">
+                  <Image 
+                    src="/images/cta.svg" 
+                    alt="Ilustração do Processo Seletivo" 
+                    layout='responsive' 
+                    width="100%" 
+                    height="100%" 
+                  />
+                </div>
+              </div>
+
+
+              <div className="col-lg-5 col-md-6 d-flex flex-column justify-content-center">
+                <h2 className="section-title">Pronto para se juntar a nós?</h2>
+                <p className="mb-4 text-justify">
+                  Confira as informações de como funciona o nosso processo seletivo.
+                </p>
+
+                <div className="mt-auto">
+                  <Link href="/prosel" passHref>
+                    <a 
+                      title="Processo Seletivo" 
+                      className="btn btn-primary">
+                      Processo Seletivo
+                    </a>
+                  </Link>
+                </div>
+              </div>
+
+
+            </div>
+          </div>
+        </section>
+      </SwiperSlide>
+
+
+
+      {/* ===== SLIDE 3: YOUTUBE ===== */}
+      <SwiperSlide>
+        <section className="section" style={{ minHeight: '80vh' }}>
+          <div className="bg-gray container section shadow rounded-lg px-4">
+            <div className="row align-items-center justify-content-center text-center text-md-left">
+
+              <div className="col-lg-6 col-md-6 d-flex flex-column justify-content-center">
+                <h2 className="section-title">PET Aulas</h2>
+                <p className="mb-4 text-justify">
+                  Confira a nossa nova paylist de <b>Instrumentação Laboratorial</b> no nosso canal do YouTube.
+                </p>
+
+                <div className="mt-auto">
+                  <a 
+                    href="https://youtube.com/playlist?list=PLPYIiTqJVmYH_u82FOhwI4T18udpdBsi-&si=Ch7VpQ5n9kXxCsHV" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    title="Acesse nossa playlist no YouTube" 
+                    className="btn btn-primary">
+                    Acessar Playlist
+                  </a>
+                </div>
+              </div>
+
+
+              <div className="col-lg-4 col-md-5 mb-4 mb-md-0">
+                <div className="img-fluid">
+                  <Image 
+                    src="/images/cta.svg" 
+                    alt="Ilustração do Processo Seletivo" 
+                    layout='responsive' 
+                    width="100%" 
+                    height="100%" 
+                  />
+                </div>
+              </div>
+
+
+            </div>
+          </div>
+        </section>
+      </SwiperSlide>
+
+
+            {/* ===== SLIDE 2:  ===== */}
+      <SwiperSlide>
         <section className="section" style={{ minHeight: '80vh' }}>
           <div className="bg-gray container section shadow rounded-lg px-4">
             <div className="row align-items-center justify-content-center text-center text-md-left">
@@ -103,10 +197,13 @@ const Carousel = () => {
                 </div>
               </div>
 
+
             </div>
           </div>
         </section>
       </SwiperSlide>
+
+
     </Swiper>
   );
 };
