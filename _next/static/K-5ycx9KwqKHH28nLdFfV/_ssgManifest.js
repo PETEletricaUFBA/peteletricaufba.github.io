9b5773a0b7e6bafa4bb23895ef1aac5833a01090
athilasilver <134936421+athilasilver@users.noreply.github.com>
2025-09-14 00:40:06 +0000

    Deploying to gh-pages from @ PETEletricaUFBA/peteletricaufba.github.io@6240934bb726eabf4cd907e8adad87a6c8bf4900 🚀

---
+++ _next/static/K-5ycx9KwqKHH28nLdFfV/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fatividades","\u002F","\u002Fcursos","\u002Fcorrente-alternativa","\u002Fmanual-calouro","\u002Fatividades\u002F[id]","\u002Fcursos\u002F[id]","\u002Fmanual-calouro\u002F[id]","\u002Fcorrente-alternativa\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
